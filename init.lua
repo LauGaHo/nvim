a37ec662ca2cd0063ed0a9a6f142bdc0ceeef949
刘嘉豪 <cattou147@icloud.com>
2022-11-25 02:27:52 +0800

    Add comment in init.lua

--- init.lua
+++ init.lua
@@ -1,7 +1,13 @@
+-- Packer 插件管理
 require("liujiahao.plugins-setup")
+-- Vim 基础配置
 require("liujiahao.core.options")
+-- Vim 键位映射
 require("liujiahao.core.keymaps")
+-- 主题设置
 require("liujiahao.core.colorscheme")
+
+-- 各插件配置
 require("liujiahao.plugins.comment")
 require("liujiahao.plugins.nvim-tree")
 require("liujiahao.plugins.lualine")
