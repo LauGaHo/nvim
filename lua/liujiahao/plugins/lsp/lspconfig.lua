1379175773604c79ab55f459e60c0f89693cd180
刘嘉豪 <cattou147@icloud.com>
2022-11-26 17:51:18 +0800

    Update(lspconfig): move the keymappings in lspconfig.lua to keymaps.lua

--- lua/liujiahao/plugins/lsp/lspconfig.lua
+++ lua/liujiahao/plugins/lsp/lspconfig.lua
@@ -16,32 +16,8 @@ if not typescript_setup then
 	return
 end
 
-local keymap = vim.keymap
-
 -- enable keybinds for available lsp server
-local on_attach = function(client, bufnr)
-	local opts = { noremap = true, silent = true, buffer = bufnr }
-
-	-- set keybinds
-	keymap.set("n", "gf", ":Lspsaga lsp_finder<CR>", opts) -- show definition, references
-	keymap.set("n", "gD", ":lua vim.lsp.buf.declaration()<CR>", opts) -- got to declaration
-	keymap.set("n", "gd", ":Lspsaga peek_definition<CR>", opts) -- see definition and make edits in window
-	keymap.set("n", "gi", ":lua vim.lsp.buf.implementation()<CR>", opts) -- go to implementation
-	keymap.set("n", "<leader>ca", ":Lspsaga code_action<CR>", opts) -- see available code actions
-	keymap.set("n", "<leader>rn", ":Lspsaga rename<CR>", opts) -- smart rename
-	keymap.set("n", "<leader>d", ":Lspsaga show_line_diagnostics<CR>", opts) -- show  diagnostics for line
-	keymap.set("n", "<leader>d", ":Lspsaga show_cursor_diagnostics<CR>", opts) -- show diagnostics for cursor
-	keymap.set("n", "[d", ":Lspsaga diagnostic_jump_prev<CR>", opts) -- jump to previous diagnostic in buffer
-	keymap.set("n", "]d", ":Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
-	keymap.set("n", "K", ":Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
-	keymap.set("n", "<leader>o", ":LSoutlineToggle<CR>", opts) -- see outline on right hand side
-
-	-- typescript specific keymaps (e.g. rename file and update imports)
-	if client.name == "tsserver" then
-		keymap.set("n", "<leader>rf", ":TypescriptRenameFile<CR>") -- rename file and update imports
-	end
-end
-
+local on_attach = require("liujiahao.core.keymaps").lspconfigList
 -- used to enable autocompletion (assign to every lsp server config)
 local capabilities = cmp_nvim_lsp.default_capabilities()
 
