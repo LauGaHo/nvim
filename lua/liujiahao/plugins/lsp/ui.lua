7b8bfa1cb8e8138dffb9039a571881aef4ae098a
Liu, Jason JH <cattou147@icloud.com>
2023-10-26 21:57:49 +0800

    feat: update ui logo

--- lua/liujiahao/plugins/lsp/ui.lua
+++ lua/liujiahao/plugins/lsp/ui.lua
@@ -4,7 +4,7 @@ vim.diagnostic.config({
 	-- 在输入模式下也更新提示，设置为 true 也许会影响性能
 	update_in_insert = true,
 })
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = "❌", Warn = "⚠️", Hint = "💡", Info = "ℹ️" }
 for type, icon in pairs(signs) do
 	local hl = "DiagnosticSign" .. type
 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
