635aebdb308c5638f4eb9808c0a1a413b992e91e
Liu, Jason JH <cattou147@icloud.com>
2023-11-07 22:55:22 +0800

    feat: update telescope version tag

--- lua/liujiahao/plugins-setup-lazy.lua
+++ lua/liujiahao/plugins-setup-lazy.lua
@@ -53,7 +53,7 @@ local plugins = {
 
 	-- fuzzy finding
 	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" }, -- dep
-	{ "nvim-telescope/telescope.nvim", tag = "0.1.1" },
+	{ "nvim-telescope/telescope.nvim", tag = "0.1.4" },
 
 	-- autocompletion
 	{
